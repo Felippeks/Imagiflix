8a3f7bc56a174a6e7d93f1c06b9fcae586875cb1
Felippe Kulkamp Sant Ana <felippeks@gmail.com>
2023-05-23 00:44:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Hero from './components/Hero';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Hero score={10}/>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App;
